193691562effa2819457a78d2f760bc1fbb411f3
Nogmanov Ildar <nogmanov@gmail.com>
2017-06-25 00:42:29 +0300

    Go out from list [bars_with_distance]

--- bars.py
+++ bars.py
@@ -4,20 +4,6 @@ from sys import argv
 from math import radians, cos, sin, asin, sqrt
 
 
-def haversine(lon1, lat1, lon2, lat2):
-    """
-    Calculate the great circle distance between two points 
-    on the earth (specified in decimal degrees)
-    """
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
-    earth_radius = 6371
-    return c * earth_radius
-
-
 def load_data(filepath):
     if not os.path.exists(filepath):
     	return None
@@ -38,15 +24,23 @@ def get_smallest_bar(data):
 
 
 def get_closest_bar(data, longitude, latitude):
-    bars_with_distance = []
-    for bar in data:
-        lon2 = bar['geoData']['coordinates'][0]
-        lat2 = bar['geoData']['coordinates'][1]
-        bars_with_distance.append([haversine(longitude, latitude, lon2, lat2), bar['Name']])
-    closest_bar = min(bars_with_distance)
-    closest_bar_name = closest_bar[1]
-    return closest_bar_name
+    def haversine(bar, longitude=longitude, latitude=latitude):
+        """
+        Calculate the great circle distance between two points
+        on the earth (specified in decimal degrees)
+        """
+        bar_longitude = bar['geoData']['coordinates'][0]
+        bar_latitude = bar['geoData']['coordinates'][1]
+        lon1, lat1, lon2, lat2 = map(radians, [longitude, latitude, bar_longitude, bar_latitude])
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+        c = 2 * asin(sqrt(a))
+        earth_radius = 6371
+        return c * earth_radius
 
+    closest_bar = min(data, key=haversine)
+    return closest_bar['Name']
 
 def handle_user_input(description):
     try:
